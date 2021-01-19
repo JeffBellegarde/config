f8a3f599695fae02489d6c4fa1b494164a731f46
Jeff Bellegarde <bellegar@gmail.com>
2021-01-18 23:30:20 -0800

    No need to run emacsclient through eval.

--- .config/fish/functions/edit.fish
+++ .config/fish/functions/edit.fish
@@ -1,9 +1,11 @@
 function edit -d "Edit a file using emacsclient"
   emacsclient -n --alternate-editor="" --eval "(if (> (length (frame-list)) 1) 't)" | grep -q t
   if test $status = 0
-     eval "emacsclient $argv"
-  else
-     eval "emacsclient -c $argv"
+     #reuse window
+     emacsclient --quiet $argv
+ else
+     #new window
+     emacsclient --quiet -c $argv
   end
 end
 
