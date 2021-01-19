99379446eef32d2ad85d9e305bd5b9eb925efc72
Jeff Bellegarde <bellegar@gmail.com>
2021-01-18 23:08:40 -0800

    Make the edit function start emacs.
    Also reuses current window if it exists.

--- .config/fish/functions/edit.fish
+++ .config/fish/functions/edit.fish
@@ -1,4 +1,9 @@
-function edit
-  eval "emacsclient $argv"
+function edit -d "Edit a file using emacsclient"
+  emacsclient -n --alternate-editor="" --eval "(if (> (length (frame-list)) 1) 't)" | grep -q t
+  if test $status = 0
+     eval "emacsclient $argv"
+  else
+     eval "emacsclient -c $argv"
+  end
 end
 
